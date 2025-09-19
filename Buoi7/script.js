9e59629ed414c741d45ff66fd5abd5fb85f6ad62
HungDevUnity <lthung93qn@gmail.com>
2025-09-19 14:14:18 +0700

    first commit

---
+++ Buoi7/script.js
@@ -0,0 +1,11 @@
+let myname = "Lê Thế Hùng";
+let age = 33;
+let isstudent = false;
+let emptyValue = null;
+let unknownValue = undefined;
+
+console.log(typeof myname); // string
+console.log(typeof age); // number
+console.log(typeof isstudent); // boolean
+console.log(typeof emptyValue); // object (this is a known quirk in JavaScript)
+console.log(typeof unknownValue); // undefined
